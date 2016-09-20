e0c0a159670be727cba4afe32fb6c4241f4508f4
M. Fay Wu <mfaywu@ucla.edu>
2016-09-20 06:50:10 -0700

    Just realised env variables can be left in meteor

--- travis_deployment.sh
+++ travis_deployment.sh
@@ -1,2 +1,2 @@
 #!/bin/bash 
-DEPLOY_HOSTNAME=us-east-1.galaxy-deploy.meteor.com meteor deploy --settings settings.json job-search.meteorapp.com
+DEPLOY_HOSTNAME=us-east-1.galaxy-deploy.meteor.com meteor deploy job-search.meteorapp.com
