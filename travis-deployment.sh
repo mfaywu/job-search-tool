ed5eb3e7e92a35b86a49a4aa3332935ae9ac5019
M. Fay Wu <mfaywu@ucla.edu>
2016-09-20 06:22:46 -0700

    Testing deployment script once again

--- travis-deployment.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-if [ "$TRAVIS_BRANCH" = "master" ]; then
-    echo "Deploying master branch to Galaxy"
-    DEPLOY_HOSTNAME=us-east-1.galaxy-deploy.meteor.com meteor deploy --settings settings.json job-search.meteorapp.com
-else
-    echo "No deployment. Branch is not master."
-fi
